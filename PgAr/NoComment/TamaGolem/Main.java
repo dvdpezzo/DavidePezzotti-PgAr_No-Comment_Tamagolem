6bb38e7c88c72b110e560aee78a083307b96b6ae
Davide <d.pezzotti005@studenti.unibs.it>
2022-05-16 09:25:00 +0200

    Aggiunta commenti al Main

--- PgAr/NoComment/TamaGolem/Main.java
+++ PgAr/NoComment/TamaGolem/Main.java
@@ -1,4 +1,24 @@
 package PgAr.NoComment.TamaGolem;
 
 public class Main {
+    //fase di setup
+    /*
+    Creazione equilibrio mondo di n elementi
+    */
+
+    //fase di scontro
+    /*
+    2 giocatori con ciascuno G tamagolem, con ciascuno P pietre( di N elementi) e vita V
+    Gioc1 schiera Tama
+    Gioc2 schiera Tama
+    I tama scagliano Pietra
+    Determinazione vincita scontro
+    Inserire questi metodi in un ciclo do while(G > 0 per entrambi i giocatori)
+     */
+
+    //fase dichiarazione vincitore
+    /*
+    vince chi ha G>0
+    Stampa equilibrio
+     */
 }
